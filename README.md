4a9936289a7d1fad6f3c38cacd69b92099f59b0a
Yosshi999 <Yosshi999@users.noreply.github.com>
2026-02-08 19:58:35 +0900

    api

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # Glued Hunyuan Video 1.5
+Glue the boundaries of latents to enable looped video generation of Diffuser-based Hunyuan Video 1.5.
+
+## System Requirements
+* Hardware: NVIDIA GPU, 14GB at minimumm
+* Software: Python >= 3.10, Diffusers >= 0.36.0, and PyTorch
+
+See [the original repository](https://github.com/Tencent-Hunyuan/HunyuanVideo-1.5?tab=readme-ov-file#-system-requirements) for detail.
+
+## Installation
+```
+git clone https://github.com/Yosshi999/GluedHunyuanVideo-1.5
+cd GluedHunyuanVideo-1.5
+pip install -e .
+```
+
+## Quick Start
+See [Usage with Diffusers](https://github.com/Tencent-Hunyuan/HunyuanVideo-1.5?tab=readme-ov-file#usage-with-diffusers) for usage of Hunyuan itself.
+
+```python
+import torch
+from diffusers import HunyuanVideo15Pipeline
+from diffusers.utils import export_to_video
+from glue_hyvideo import patch_pipeline
+
+dtype = torch.bfloat16
+device = "cuda:0"
+seed = 42
+prompt = "A cat is rolling forward and then looking around. The scene is in an animated style."
+
+pipe = HunyuanVideo15Pipeline.from_pretrained("hunyuanvideo-community/HunyuanVideo-1.5-Diffusers-720p_t2v", torch_dtype=dtype)
+pipe.enable_model_cpu_offload()
+pipe.vae.enable_tiling()
+
+patch_pipeline(pipe, glued_dims=(True, True, False))
+
+generator = torch.Generator(device=device).manual_seed(seed)
+
+video = pipe(
+    prompt=prompt,
+    generator=generator,
+    num_frames=121,
+    num_inference_steps=50,
+).frames[0]
+
+export_to_video(video, "output.mp4", fps=24)
+```
--- glue_hyvideo/patch.py
+++ glue_hyvideo/patch.py
@@ -6,12 +6,25 @@ from .gsta import GluedSlidingTiledFlexAttnProcessor
 
 def patch_pipeline(
     pipe: HunyuanVideo15Pipeline,
+    glued_dims: Tuple[bool, bool, bool] = (True, False, False),
     tile_dims: Tuple[int, int, int] = (4, 8, 8),
-    kernel_dims: Tuple[int, int, int] = (1, 1, 1),
-    glued_dims: Tuple[bool, bool, bool] = (False, False, False),
+    kernel_dims: Optional[Tuple[int, int, int]] = None,
     rope_dim_list: Tuple[int, int, int] = (16, 56, 56),
     rope_theta: int = 256,
 ) -> None:
+    """Patches HunyuanVideo15Pipeline in-place to use Glued Sliding Tiled Attention (GSTA).
+    Args:
+        pipe (HunyuanVideo15Pipeline): The pipeline to be patched.
+        glued_dims (Tuple[bool, bool, bool]): A tuple indicating which dimensions to apply gluing (temporal, height, width).
+        tile_dims (Tuple[int, int, int]): The dimensions of the tiles (temporal, height, width).
+            As it gets larger, the computation gets more efficient but the quality may degrade.
+        kernel_dims (Optional[Tuple[int, int, int]]): The dimensions of the attention kernel (temporal, height, width).
+            If None, it gets as large as possible.
+            As it gets smaller, the computation gets more efficient but the quality may degrade.
+        rope_dim_list (Tuple[int, int, int]): The dimensions for RoPE (temporal, height, width).
+        rope_theta (int): The theta parameter for RoPE.
+    """
+
     assert pipe.__class__.__name__ == "HunyuanVideo15Pipeline", "This patch function only works for HunyuanVideo15Pipeline."
     if getattr(pipe, "__patched_gsta__", False):
         # already patched
@@ -48,11 +61,19 @@ def patch_pipeline(
                 self.default_aspect_ratio[1], self.default_aspect_ratio[0], self.target_size
             )
         canvas_dims =(
-            (num_frames - 1) // self.vae_scale_factor_temporal + 1,
-            int(height) // self.vae_scale_factor,
-            int(width) // self.vae_scale_factor,
+            (num_frames - 1) // int(self.vae_scale_factor_temporal) + 1,
+            int(height) // int(self.vae_scale_factor),
+            int(width) // int(self.vae_scale_factor),
         )
-        processor = GluedSlidingTiledFlexAttnProcessor(canvas_dims, tile_dims, kernel_dims, glued_dims, rope_dim_list, rope_theta)
+        if kernel_dims is None:
+            _kernel_dims = (
+                canvas_dims[0] // tile_dims[0],
+                canvas_dims[1] // tile_dims[1],
+                canvas_dims[2] // tile_dims[2],
+            )
+        else:
+            _kernel_dims = kernel_dims
+        processor = GluedSlidingTiledFlexAttnProcessor(canvas_dims, tile_dims, _kernel_dims, glued_dims, rope_dim_list, rope_theta)
         for block in self.transformer.transformer_blocks:
             block.attn.set_processor(processor)
         
